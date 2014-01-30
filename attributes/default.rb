5de9eb7744cefc7158399cb99373e40f85982820
Fraser Scott <admin@thunr.com>
2014-01-30 13:38:46 +0000

    db not playing nicely since changes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['thunr-rails']['db']['adapter'] = 'sqlite3'
-default['thunr-rails']['db']['host'] = nil
-default['thunr-rails']['db']['database'] = 'db/thunr-rails.sqlite'
+default['thunr-rails']['db']['adapter'] = 'mysql'
+default['thunr-rails']['db']['host'] = '127.0.0.1'
+default['thunr-rails']['db']['database'] = 'thunr-db'
 default['thunr-rails']['db']['username'] = 'thunr-db'
 default['thunr-rails']['db']['password'] = 'changeme'
