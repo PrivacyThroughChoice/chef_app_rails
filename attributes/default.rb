bb9a89af04b8fb3dc48bab6095a0e90286e5f4c6
Fraser Scott <admin@thunr.com>
2014-01-30 11:57:04 +0000

    test password mismatch

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['thunr-rails']['db']['adapter'] = 'sqlite3'
 default['thunr-rails']['db']['host'] = nil
 default['thunr-rails']['db']['database'] = 'db/thunr-rails.sqlite'
 default['thunr-rails']['db']['username'] = 'thunr-db'
-default['thunr-rails']['db']['password'] = 'pleasechangeme'
+default['thunr-rails']['db']['password'] = 'changeme'
